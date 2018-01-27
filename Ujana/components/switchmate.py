5562ac2a1309bffae843d0784f537f202bec42a6
Reed Slaby <reed.slaby@gmail.com>
2018-01-26 22:28:08 -0600

    Fixed system time setting

--- Ujana/components/switchmate.py
+++ Ujana/components/switchmate.py
@@ -19,20 +19,24 @@ class Bright:
     def _connect(self):
         return bluepy.btle.Peripheral(self.mac, bluepy.btle.ADDR_TYPE_RANDOM, 0)
 
+    def SetMotion(self, enable="yes", start="00:00", end="00:00", duration="00:01"):
+        lightswitch = self._connect()
+
+        hexhour = datetime.datetime.now().hour.to_bytes(1, "big")
+        hexmin = datetime.datetime.now().minute.to_bytes(1, "big")
+        hexsec = datetime.datetime.now().second.to_bytes(1, "big")
+        hexday = datetime.datetime.now().weekday().to_bytes(1, "big")
+
+        writeval = hexhour + hexmin + hexsec + hexday
+
+        print(str(writeval))
+
+        lightswitch.writeCharacteristic(self.handles['SysTime'], writeval)
+        lightswitch.writeCharacteristic(self.handles['Motion'], b'\x01\x00\x00\x00\x00\x01')
+
     def SetState(self, cmd):
         lightswitch = self._connect()
         if (cmd == "on"):
-            lightswitch.writeCharacteristic(0x0030, b'\x01')
+            lightswitch.writeCharacteristic(self.handles['SwState'], b'\x01')
         else:
-            lightswitch.writeCharacteristic(0x0030, b'\x00')
-
-    def SetMotion(self, enable="yes", start="00:00", end="00:00", duration="00:01"):
-        lightswitch = self._connect()
-        hexhour = datetime.datetime.now().hour.to_bytes(1,"big")
-        hexmin = datetime.datetime.now().minute.to_bytes(1, "big")
-        hexsec = datetime.datetime.now().second.to_bytes(1, "big")
-        hexusec = b'\x01'
-        writeval = hexhour + hexmin + hexsec + hexusec
-        print(writeval)
-        lightswitch.writeCharacteristic(self.handles['SysTime'],writeval)
-        lightswitch.writeCharacteristic(self.handles['Motion'],b'\x01\x00\x00\x00\x00\x01')
+            lightswitch.writeCharacteristic(self.handles['SwState'], b'\x00')
