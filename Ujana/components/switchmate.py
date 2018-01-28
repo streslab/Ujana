f8e9a73e7b04833fc8a691a5e90802c8fa0d51a0
Reed Slaby <reed.slaby@gmail.com>
2018-01-27 23:15:39 -0600

    Motion setting improvements

--- Ujana/components/switchmate.py
+++ Ujana/components/switchmate.py
@@ -19,20 +19,27 @@ class Bright:
     def _connect(self):
         return bluepy.btle.Peripheral(self.mac, bluepy.btle.ADDR_TYPE_RANDOM, 0)
 
-    def SetMotion(self, enable="yes", start="00:00", end="00:00", duration="00:01"):
-        lightswitch = self._connect()
-
+    def _sysbytetime(self):
         hexhour = datetime.datetime.now().hour.to_bytes(1, "big")
         hexmin = datetime.datetime.now().minute.to_bytes(1, "big")
         hexsec = datetime.datetime.now().second.to_bytes(1, "big")
+        return hexhour + hexmin + hexsec
+
+    def SetMotion(self, enable="yes", start="00:00", end="00:00", duration="00:01"):
+        lightswitch = self._connect()
+
+        hextime = self._sysbytetime()
         hexday = datetime.datetime.now().weekday().to_bytes(1, "big")
 
-        writeval = hexhour + hexmin + hexsec + hexday
+        currenttime = hextime + hexday
 
-        print(str(writeval))
+        if enable == "yes":
+            motion = b'\x01'
+        else:
+            motion = b'\x00'
 
-        lightswitch.writeCharacteristic(self.handles['SysTime'], writeval)
-        lightswitch.writeCharacteristic(self.handles['Motion'], b'\x01\x00\x00\x00\x00\x01')
+        lightswitch.writeCharacteristic(self.handles['SysTime'], currenttime)
+        lightswitch.writeCharacteristic(self.handles['Motion'], motion + b'\x00\x00\x00\x00\x01')
 
     def SetState(self, cmd):
         lightswitch = self._connect()
